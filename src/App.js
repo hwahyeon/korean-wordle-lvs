7771e15fa2f859f2c99b201fa80378001ac7ef38
hwahyeon <hwahyeon@knou.ac.kr>
2023-07-22 21:51:44 +0900

    file이름 변경 Home -> WordleKor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Pages/Home.js';
+import WordleKor from './components/Pages/WordleKor.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<WordleKor />} />
       </Routes>
     </Router>
   );
