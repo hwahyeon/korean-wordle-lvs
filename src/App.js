23cc3b1b1ae01e98931881d35b08e466e3e1ffc9
hwahyeon <hwahyeon@knou.ac.kr>
2023-07-22 19:27:34 +0900

    키보드 구축

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Pages/Home.js';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Box, Button } from '@mui/material';
+
+
+function HomePage() {
+  return (
+    <Container>
+      <Box>
+        <input></input>
+      </Box>
+      <Box>
+        <Button>ㅂ</Button>
+        <Button>ㅈ</Button>
+        <Button>ㄷ</Button>
+        <Button>ㄱ</Button>
+        <Button>ㅅ</Button>
+        <Button>ㅛ</Button>
+        <Button>ㅕ</Button>
+        <Button>ㅑ</Button>
+        <Button>ㅐ</Button>
+        <Button>ㅔ</Button>
+      </Box>
+      <Box>
+        <Button>ㅁ</Button>
+        <Button>ㄴ</Button>
+        <Button>ㅇ</Button>
+        <Button>ㄹ</Button>
+        <Button>ㅎ</Button>
+        <Button>ㅗ</Button>
+        <Button>ㅓ</Button>
+        <Button>ㅏ</Button>
+        <Button>ㅣ</Button>
+      </Box>
+      <Box>
+        <Button>Enter</Button>
+        <Button>ㅋ</Button>
+        <Button>ㅌ</Button>
+        <Button>ㅊ</Button>
+        <Button>ㅍ</Button>
+        <Button>ㅠ</Button>
+        <Button>ㅜ</Button>
+        <Button>ㅡ</Button>
+        <Button>Remove</Button>
+      </Box>
+    </Container>
+  );
+}
+
+export default HomePage;
