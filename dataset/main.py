c06be52e2bba9b0adb3b097d8eda205e12569160
hwahyeon <hwahyeon@knou.ac.kr>
2023-09-09 00:33:22 +0900

    Add the funtion for making JSON

--- dataset/main.py
+++ dataset/main.py
@@ -7,6 +7,24 @@ os.chdir('dataset/xls')
 lst_xls = os.listdir()
 
 
+def add_to_json(new_set, file_path='dataset.json'):
+    try:
+        # 기존 데이터 불러오기
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            existing_data = json.load(json_file)
+    except FileNotFoundError:
+        # 파일이 없으면 새로운 리스트 생성
+        existing_data = []
+
+    # 새 데이터 추가
+    existing_data.extend(list(new_set))
+
+    # 데이터를 JSON 파일로 다시 저장
+    with open(file_path, 'w', encoding='utf-8') as json_file:
+        json.dump(existing_data, json_file, ensure_ascii=False, indent=4)
+
+
+
 for i in lst_xls:
     result_set = set()
 
@@ -25,9 +43,8 @@ for i in lst_xls:
     wordle_list = []
 
     for j in result_set:
-        if len(hangul_decompose(j)) == 5:
-            wordle_list.append(j)
-
-    # list를 JSON 파일로 저장
-    with open(f'my_data{i}.json', 'w', encoding='utf-8') as json_file:
-        json.dump(wordle_list, json_file, ensure_ascii=False, indent=4)
+        word = hangul_decompose(j)
+        if len(word) == 5:
+            wordle_list.append(word)
+    
+    add_to_json(wordle_list)
