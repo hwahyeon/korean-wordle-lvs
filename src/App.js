ce61a90daf402115e720672cc53540ef6887ecfe
hwahyeon <hwahyeon@knou.ac.kr>
2023-09-09 03:02:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Pages/Home.js';
 import WordleKor from './components/Pages/WordleKor.js';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<WordleKor />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/play" element={<WordleKor />} />
       </Routes>
     </Router>
   );
