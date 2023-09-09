d5501b425217466b0dec662ce24bc29bde502259
hwahyeon <hwahyeon@knou.ac.kr>
2023-09-09 16:58:08 +0900

    Update Homepage.js

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container, Box, Button, Typography as P } from '@mui/material';
+import '../../styles/home.scss';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -29,13 +30,16 @@ function HomePage() {
   }, []);
 
   return (
-    <Container>
-      <P>한글 Korean Wordle</P>
-      <Button onClick={handleClick}>시작해볼까요?</Button>
-      <Box>
-        <P>이 게임은 Wordle의 한글 변형판입니다. 영어로 된 오리지널 Wordle은 여기로. 한글 Korean Wordle의 소스코드는 여기로</P>
-        <P>{currentDateTime.date}</P>
-        <P>Edited by Hwahyeon</P>
+    <Container className='HomePage'>
+      <Box className='content'>
+        <P className='header'>한글 Korean Wordle</P>
+        <Button className='play_button' onClick={handleClick}>시작해볼까요?</Button>
+        <Box>
+          <P>이 게임은 Wordle의 한글 변형판입니다</P>
+          <P>소스코드는 이쪽, 영어로 된 오리지널 Wordle은 여기로</P>
+          <P className='date'>{currentDateTime.date}</P>
+          <P className='edit'>Edited by hwahyeon</P>
+        </Box>
       </Box>
     </Container>
   );
