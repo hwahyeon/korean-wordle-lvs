be1f88675528473975e8479b6db29da8f622d27a
hwahyeon <hwahyeon@knou.ac.kr>
2023-09-09 16:58:20 +0900

    Change a route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/play" element={<WordleKor />} />
       </Routes>
     </Router>
