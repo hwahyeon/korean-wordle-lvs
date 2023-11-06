28648e9c1ffa0f0165da8d2f95577be70a446bea
hwahyeon <hwahyeon@knou.ac.kr>
2023-11-06 22:42:46 +0900

    Added the function of imdt mode

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,19 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import { Container, Box, Button, Typography as P } from '@mui/material';
-import '../../styles/home.scss';
-import icon from '../../assets/wordleicon.svg';
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Container, Box, Typography as P } from "@mui/material";
+import "../../styles/home.scss";
+import icon from "../../assets/wordleicon.svg";
 import Header from "../Common/Header";
 
 function HomePage() {
   const navigate = useNavigate();
 
   const handleEasyClick = () => {
-    navigate('/play/easy');
+    navigate("/play/easy");
+  };
+
+  const handleImdtClick = () => {
+    // navigate('/play/imdt');
+    alert("준비중입니다");
   };
 
   const handleHardClick = () => {
-    navigate('/play/hard');
+    navigate("/play/hard");
   };
 
   const [currentDateTime, setCurrentDateTime] = useState(() => {
@@ -36,35 +41,43 @@ function HomePage() {
   }, []);
 
   return (
-    <Container className='HomePage'>
+    <Container className="HomePage">
       <Header />
-      <Box className='content'>
-        <img src={icon} alt="icon" className='icon' />
-        <Box>시작해볼까요?
-          <br></br>
-          <Button className='play_button' onClick={handleEasyClick}>낮은 난이도</Button>
-          <Button className='play_button' onClick={handleHardClick}>높은 난이도</Button>
+      <Box className="content">
+        <img src={icon} alt="icon" className="icon" />
+        <p className="cont_txt">이 게임은 Wordle의 한글 변형판입니다</p>
+        <Box>
+          <p className="start_text">시작해볼까요?</p>
+          <p className="level_text">난이도를 선택하세요</p>
+          <button className="play_button" onClick={handleEasyClick}>
+            초급
+          </button>
+          <button className="play_button" onClick={handleImdtClick}>
+            중급
+          </button>
+          <button className="play_button" onClick={handleHardClick}>
+            고급
+          </button>
         </Box>
         <Box>
-          <P className='cont_txt'>이 게임은 Wordle의 한글 변형판입니다</P>
-          <Button
-            className='alink_button'
+          <P className="date">{currentDateTime.date}</P>
+          <P className="edit">Made by hwahyeon</P>
+        </Box>
+        <Box className="ref">
+          <button
+            className="alink_button"
             component={Link}
             to="https://github.com/hwahyeon/reactjs-wordle-kor"
           >
-            Source of this web
-          </Button>
-          <Button
-            className='alink_button'
+            Code of this game
+          </button>
+          <button
+            className="alink_button"
             component={Link}
             to="https://www.nytimes.com/games/wordle/index.html"
           >
             Original Wordle
-          </Button>
-        </Box>
-        <Box>
-          <P className='date'>{currentDateTime.date}</P>
-          <P className='edit'>Made by hwahyeon</P>
+          </button>
         </Box>
       </Box>
     </Container>
