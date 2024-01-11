f8dc5553d3ca58b1e54a280b5d7aab838e127429
hwahyeon <hwahyeon@knou.ac.kr>
2024-01-12 02:27:04 +0900

    Remove non use import ones

--- src/components/Common/CentralMessage.js
+++ src/components/Common/CentralMessage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../../styles/components/_central-message.scss'
 
 function CentralMessage({ message, duration }) {
