305d759912c463c0e108145c5686e1539a5018f9
hwahyeon <hwahyeon@knou.ac.kr>
2024-01-12 02:28:55 +0900

    Add colorMode state

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,20 +1,32 @@
-import React from "react";
+// React
+import React, { useEffect } from "react";
 import { useState } from "react";
-import "../../styles/header.scss";
 import { useNavigate } from "react-router-dom";
+
+// State
+import { useRecoilState } from "recoil";
+import { darkModeState, colorModeState } from "../../state/themeState.js";
+
+// Style
+import "../../styles/components/_header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faInfoCircle,
   faSun,
   faMoon,
+  faAdjust,
+  faEye,
 } from "@fortawesome/free-solid-svg-icons";
+
+// Components
 import InfoModal from "../Common/InfoModal.js";
 
 function Header() {
   const navi = useNavigate();
   const [showInfoModal, setShowInfoModal] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useRecoilState(darkModeState);
+  const [colorMode, setColorMode] = useRecoilState(colorModeState);
 
   const goHome = () => {
     navi("/");
@@ -29,23 +41,46 @@ function Header() {
   };
 
   const toggleDarkMode = () => {
-    alert("다크모드는 준비중입니다.");
-    setDarkMode(!darkMode)
+    setDarkMode(!darkMode);
   };
+
+  const toggleColorMode = () => {
+    setColorMode(!colorMode);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
+
+  useEffect(() => {
+    if (colorMode) {
+      document.body.classList.add("color-mode");
+    } else {
+      document.body.classList.remove("color-mode");
+    }
+  }, [colorMode]);
 
   return (
     <header className="header">
-      <div className="header__icon" onClick={goHome}>
+      <div className="header__firsticon" onClick={goHome}>
         <FontAwesomeIcon icon={faHome} />
       </div>
+      <div class="spacer"></div>
       <div className="header__title">
         <p>한글 Wordle</p>
       </div>
-      <div className="header__group">
-        <div onClick={openInfoModal}>
+      <div className="header__secondicon">
+        <div className="icon-items" onClick={openInfoModal}>
           <FontAwesomeIcon icon={faInfoCircle} />
         </div>
-        <div onClick={toggleDarkMode}>
+        <div className="icon-items" onClick={toggleColorMode}>
+          <FontAwesomeIcon icon={!colorMode ? faAdjust : faEye} />
+        </div>
+        <div className="icon-items" onClick={toggleDarkMode}>
           <FontAwesomeIcon icon={!darkMode ? faMoon : faSun} />
         </div>
       </div>
