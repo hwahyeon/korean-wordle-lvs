8075f941b5fbe4d838d59ff0a8304fe188484b6d
hwahyeon <hwahyeon@knou.ac.kr>
2024-01-12 02:10:37 +0900

    Add color mode state

--- src/state/themeState.js
+++ src/state/themeState.js
@@ -4,3 +4,8 @@ export const darkModeState = atom({
   key: 'darkModeState',
   default: false, // 기본값은 false (라이트 모드)
 });
+
+export const colorModeState = atom({
+  key: 'colorModeState',
+  default: false, // 기본값은 false (색맹 모드 비활성화)
+});
