a72fd96e42faddc55e2d2148bddb0650bca7f0c7
hwahyeon <hwahyeon@knou.ac.kr>
2024-01-22 17:45:11 +0900

    Add sub explain and Format with prettier for styling

--- src/components/ModalPages/InfoPageFour.js
+++ src/components/ModalPages/InfoPageFour.js
@@ -1,60 +1,58 @@
 const InfoPageFour = () => {
   return (
     <div className="example">
-    <p className="example__header">게임하는 방법</p>
-    <p className="example__explain">
-      6번 시도해서 숨겨진 글자(명사)를 맞춰보세요
-    </p>
-    <p className="example__sub-header">색상은 정답의 단서가 됩니다.</p>
-    <div className="example__group">
-      <div className="example__div">
-        <div className="gray">ㅎ</div>
-        <div className="yellow">ㅏ</div>
-        <div className="gray">ㄴ</div>
-        <div className="gray">ㅡ</div>
-        <div className="gray">ㄹ</div>
+      <p className="example__header">게임하는 방법</p>
+      <p className="example__with">함께 해볼까요? (3/5)</p>
+      <div className="example__group">
+        <div className="example__div">
+          <div className="gray">ㅎ</div>
+          <div className="yellow">ㅏ</div>
+          <div className="gray">ㄴ</div>
+          <div className="gray">ㅡ</div>
+          <div className="gray">ㄹ</div>
+        </div>
+        <div className="example__div">
+          <div className="gray btn--active">ㅂ</div>
+          <div className="gray btn--active">ㅗ</div>
+          <div className="yellow btn--active">ㄱ</div>
+          <div className="green btn--active">ㅅ</div>
+          <div className="green btn--active">ㅏ</div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p className="example__text-explain">
+          이런, 정답은 아니지만 확실한 단서를 얻었어요. 오늘의 단어는{" "}
+          <span>사</span>로 끝나는 단어였군요. <br />
+          음, <span>경사</span>로 한 번 시도해 볼까요?
+        </p>
       </div>
-      <div className="example__div">
-        <div className="gray btn--active">ㅂ</div>
-        <div className="gray btn--active">ㅗ</div>
-        <div className="yellow btn--active">ㄱ</div>
-        <div className="green btn--active">ㅅ</div>
-        <div className="green btn--active">ㅏ</div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <p className="example__text-explain">
-      정답에 가까워지고 있습니다. 오늘의 단어는 사로 끝나는 단어였군요. 음, 경사로 한 번 시도해보죠.
-      </p>
-
     </div>
-  </div>
   );
 };
 
--- src/components/ModalPages/InfoPageTwo.js
+++ src/components/ModalPages/InfoPageTwo.js
@@ -1,60 +1,57 @@
 const InfoPageTwo = () => {
   return (
     <div className="example">
-    <p className="example__header">게임하는 방법</p>
-    <p className="example__explain">
-      6번 시도해서 숨겨진 글자(명사)를 맞춰보세요
-    </p>
-    <p className="example__sub-header">색상은 정답의 단서가 됩니다.</p>
-    <div className="example__group">
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <p className="example__header">게임하는 방법</p>
+      <p className="example__with">함께 해볼까요? (1/5)</p>
+      <div className="example__group">
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="example__div">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p className="example__text-explain">
+          처음 시작화면입니다. 지금은 아무 단서가 없으니 아무 단어나 써볼까요?{" "}
+          <span>하늘</span>을 써보겠습니다.
+        </p>
       </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="example__div">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <p className="example__text-explain">
-      처음 시작화면입니다. 지금은 아무 단서가 없으니 아무 단어나 써볼까요? 하늘이 좋겠군요.
-      </p>
-
     </div>
-  </div>
   );
 };
 
