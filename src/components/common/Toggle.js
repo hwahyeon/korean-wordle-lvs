1121b545f3a010ef6a9c80d269e4ab67a834c848
hwahyeon <hwahyeon@knou.ac.kr>
2024-02-19 02:31:32 +0900

    Create toggle component

---
+++ src/components/common/Toggle.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "../../styles/components/_toggle.scss";
+
+const Toggle = ({title, description, isOn, onChange}) => {
+
+  return (
+    <div className="toggle-wrapper">
+      <div className="desc">
+        <strong>{title}</strong>
+        <p>{description}</p>
+      </div>
+      <div className={`toggle-container ${isOn ? "toggle--checked" : ""}`} onClick={onChange}>
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : ""}`}/>
+      </div>
+    </div>
+  );
+};
+
+export default Toggle;
