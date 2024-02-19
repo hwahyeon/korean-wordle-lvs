366b08cccc0e890e2783404e04520a9253962b54
hwahyeon <hwahyeon@knou.ac.kr>
2024-02-19 19:17:05 +0900

    Add globalState for language

--- src/state/sidebarState.js
+++ src/state/sidebarState.js
@@ -4,3 +4,8 @@ export const sidebarState = atom({
   key: 'sidebarState',
   default: false, // 초기 값 (사이드바 닫힘)
 });
+
+export const currentLanguage = atom({
+  key: 'languageState',
+  default: false, // 초기 값 (한국어)
+});
